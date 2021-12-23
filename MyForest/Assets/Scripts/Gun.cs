ed1fb1ee8086b0420d12311110ab0be26bcbea7e
BeXreR <berkayymuratt@gmail.com>
2021-12-24 00:11:34 +0300

    .

--- MyForest/Assets/Scripts/Gun.cs
+++ MyForest/Assets/Scripts/Gun.cs
@@ -6,7 +6,7 @@ using UnityEngine.Serialization;
 
 public class Gun : MonoBehaviour
 {
-    private Animation m_RecoilAnimation; 
+    private Animation m_Animation; 
     private AudioSource m_AudioSource;
     
     public AudioClip shootingSound;
@@ -14,7 +14,7 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_RecoilAnimation = gameObject.GetComponent<Animation>();
+        m_Animation = gameObject.GetComponent<Animation>();
         m_AudioSource = gameObject.GetComponent<AudioSource>();
     }
 
@@ -26,8 +26,11 @@ public class Gun : MonoBehaviour
 
     public void Shoot()
     {
-        PlayShootingSound();
-        m_RecoilAnimation.Play();
+        if (!m_Animation.IsPlaying("gun-recoil"))
+        {
+            PlayShootingSound();
+            m_Animation.Play("gun-recoil");
+        }
     }
 
     private void PlayShootingSound()
