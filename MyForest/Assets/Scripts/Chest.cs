0bcc09a5769277c383a4eeffa8dfa3971fddc194
BeXreR <berkayymuratt@gmail.com>
2021-12-25 19:02:51 +0300

    .

--- MyForest/Assets/Scripts/Chest.cs
+++ MyForest/Assets/Scripts/Chest.cs
@@ -6,28 +6,51 @@ using UnityEngine.UI;
 
 public class Chest : MonoBehaviour
 {
+    public String chestType;
     
-    private Text m_InteractionText;
-    private Hero m_Hero;
     private bool m_InChestArea;
+    private bool m_IsChestOpened;
+    private Light m_SpotLight;
+    private Text m_InteractionText;
+
+    private Hero m_Hero;
 
     private void Start()
     {
         m_Hero = GameObject.Find("Hero").GetComponent<Hero>();
         m_InteractionText = GameObject.Find("InteractionText").GetComponent<Text>();
+        m_SpotLight = gameObject.GetComponentInChildren<Light>();
     }
 
     private void Update()
     {
-        if (m_InChestArea)
+        if (m_InChestArea && !m_IsChestOpened)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log("Pressed to E..");
-                ClearText();
-                Destroy(gameObject);
+                OpenChest();
             }
         }
+    }
+
+    public void OpenChest()
+    {
+        switch (chestType)
+        {
+            case "MedkitChest":
+                m_Hero.getMedkit();
+                break;
+            case "AmmoChest":
+                m_Hero.getAmmo();
+                break;
+            case "KeyChest":
+                m_Hero.getKey();
+                break;
+        }
+        
+        m_IsChestOpened = true;
+        ClearText();
+        m_SpotLight.enabled = false;
     }
 
     public void ChangeText()
@@ -42,7 +65,7 @@ public class Chest : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name.Equals("Hero"))
+        if (other.gameObject.name.Equals("Hero") && !m_IsChestOpened)
         {
             m_InChestArea = true;
             ChangeText();
@@ -51,7 +74,7 @@ public class Chest : MonoBehaviour
     
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name.Equals("Hero"))
+        if (other.gameObject.name.Equals("Hero") && !m_IsChestOpened)
         {
             m_InChestArea = false;
             ClearText();
--- MyForest/Assets/Scripts/Hero.cs
+++ MyForest/Assets/Scripts/Hero.cs
@@ -7,9 +7,14 @@ public class Hero : MonoBehaviour
     public Gun gun;
     public int health;
 
+    private int m_MedkitCount;
+    private int m_KeyCount;
+
     // Update is called once per frame
     void Start()
     {
+        m_MedkitCount = 0;
+        m_KeyCount = 0;
         health = 100;
     }
 
@@ -19,10 +24,29 @@ public class Hero : MonoBehaviour
         {
             gun.Shoot();
         }
+        
+        Debug.Log("Medkit: " + m_MedkitCount);
+        Debug.Log("Ammo: " + gun.m_bulletCount);
+        Debug.Log("Key: " + m_KeyCount);
     }
 
     public void getDamage(int value)
     {
         health -= value;
+    }
+
+    public void getMedkit()
+    {
+        m_MedkitCount++;
+    }
+
+    public void getAmmo()
+    {
+        gun.m_bulletCount += 5;
+    }
+
+    public void getKey()
+    {
+        m_KeyCount++;
     }
 }
