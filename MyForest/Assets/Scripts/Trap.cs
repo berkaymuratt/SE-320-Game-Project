dfc71d156225061849c9a6e6ebac14cea1614904
BeXreR <berkayymuratt@gmail.com>
2022-01-27 16:33:24 +0300

    .

--- MyForest/Assets/Scripts/Trap.cs
+++ MyForest/Assets/Scripts/Trap.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     private Hero m_Hero;
+    private AudioSource AudioSource;
     
     void Start()
     {
         m_Hero = GameObject.Find("Hero").GetComponent<Hero>();
+        AudioSource = gameObject.GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Equals("Hero"))
         {
+            AudioSource.Play();
             m_Hero.GetDamage(20);
         }
     }
